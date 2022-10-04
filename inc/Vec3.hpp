ac04d976005ac57ba30477d5fb4a8562d387bfc5
Michael Ly <71558719+Michael-Q-Ly@users.noreply.github.com>
2022-10-03 20:30:19 -0500

    Fix inline Vec3 inline functions
    	- Some parameters needed to be referenced
    	- inline functions must be defined in the header; they cannot be
    	  prototypes

--- inc/Vec3.hpp
+++ inc/Vec3.hpp
@@ -40,19 +40,157 @@ class Vec3 {
 using point3 = Vec3 ;           // 3D point
 using color  = Vec3 ;           // RGB color
 
-
 /*-----------------------------------------------------------------------------
  * Vec3 Utility Functions 
  *-----------------------------------------------------------------------------*/
-inline std::ostream &operator<<(std::ostream &out, Vec3 const &v) ;
-inline Vec3 operator+(Vec3 const &u, Vec3 const v) ;
-inline Vec3 operator-(Vec3 const &u, Vec3 const v) ;
-inline Vec3 operator*(Vec3 const &u, Vec3 const v) ;
-inline Vec3 operator*(double t, Vec3 const v) ;
-inline Vec3 operator*(Vec3 const &v, double t) ;
-inline Vec3 operator/(Vec3 v, double t) ;
-inline double dot(Vec3 const &u, Vec3 const &v) ;
-inline Vec3 cross(Vec3 const &u, Vec3 const &v) ;
-inline Vec3 unit_vector(Vec3 v) ;
+
+
+/* ----------------------------------------------------------------------------*/
+/**
+ * @Brief 
+ *
+ * @Param out
+ * @Param v
+ *
+ * @Returns 
+ */
+/* ------------------------------------------------------------------------------------*/
+inline std::ostream &operator<<(std::ostream &out, Vec3 const &v) {
+        return (out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2]) ;
+}
+
+/* ----------------------------------------------------------------------------*/
+/**
+ * @Brief 
+ *
+ * @Param u
+ * @Param v
+ *
+ * @Returns 
+ */
+/* ------------------------------------------------------------------------------------*/
+inline Vec3 operator+(Vec3 const &u, Vec3 const &v) {
+        return (Vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2])) ;
+}
+
+/* ----------------------------------------------------------------------------*/
+/**
+ * @Brief 
+ *
+ * @Param u
+ * @Param v
+ *
+ * @Returns 
+ */
+/* ------------------------------------------------------------------------------------*/
+inline Vec3 operator-(Vec3 const &u, Vec3 const &v) {
+        return (Vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2])) ;
+}
+
+/* ----------------------------------------------------------------------------*/
+/**
+ * @Brief 
+ *
+ * @Param u
+ * @Param v
+ *
+ * @Returns 
+ */
+/* ------------------------------------------------------------------------------------*/
+inline Vec3 operator*(Vec3 const &u, Vec3 const &v) {
+        return (Vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2])) ;
+}
+
+/* ----------------------------------------------------------------------------*/
+/**
+ * @Brief 
+ *
+ * @Param t
+ * @Param v
+ *
+ * @Returns 
+ */
+/* ------------------------------------------------------------------------------------*/
+inline Vec3 operator*(double t, Vec3 const &v) {
+        return (Vec3(t * v.e[0], t * v.e[1], t * v.e[2])) ;
+}
+
+/* ----------------------------------------------------------------------------*/
+/**
+ * @Brief 
+ *
+ * @Param v
+ * @Param t
+ *
+ * @Returns 
+ */
+/* ------------------------------------------------------------------------------------*/
+inline Vec3 operator*(Vec3 const &v, double t) {
+        return (t * v) ;
+}
+
+/* ----------------------------------------------------------------------------*/
+/**
+ * @Brief 
+ *
+ * @Param v
+ * @Param t
+ *
+ * @Returns 
+ */
+/* ------------------------------------------------------------------------------------*/
+inline Vec3 operator/(Vec3 v, double t) {
+        return ((1 / t) * v) ;
+}
+
+/* ----------------------------------------------------------------------------*/
+/**
+ * @Brief 
+ *
+ * @Param u
+ * @Param v
+ *
+ * @Returns 
+ */
+/* ------------------------------------------------------------------------------------*/
+inline double dot(Vec3 const &u, Vec3 const &v) {
+        return (
+                u.e[0] * v.e[0]
+                + u.e[1] * v.e[1]
+                + u.e[2] * v.e[2]
+        ) ;
+}
+
+/* ----------------------------------------------------------------------------*/
+/**
+ * @Brief 
+ *
+ * @Param u
+ * @Param v
+ *
+ * @Returns 
+ */
+/* ------------------------------------------------------------------------------------*/
+inline Vec3 cross(Vec3 const &u, Vec3 const &v) {
+        return (
+                Vec3(
+                        (u.e[1] * v.e[2]) - (u.e[2] * v.e[1]),
+                        (u.e[2] * v.e[0]) - (u.e[0] * v.e[2]),
+                        (u.e[0] * v.e[1]) - (u.e[1] * v.e[0])
+                )
+        ) ;
+}
+/* ----------------------------------------------------------------------------*/
+/**
+ * @Brief 
+ *
+ * @Param v
+ *
+ * @Returns 
+ */
+/* ------------------------------------------------------------------------------------*/
+inline Vec3 unit_vector(Vec3 v) {
+        return (v / v.length()) ;
+}
 
 #endif /* _VEC3_HPP_ */
