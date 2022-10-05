b8e3cb071d2c2ac768e4013edb6d391723e2c926
Michael Ly <71558719+Michael-Q-Ly@users.noreply.github.com>
2022-10-04 19:18:45 -0500

    Modify hit_sphere to shade sphere
    	- hit_sphere now returns a double and checks if the ray is
    	  inside or outside the sphere
    	- ray_color updated to match this

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,10 @@
 #include "Vec3.hpp"
 #include <iostream>
 
-static bool hit_sphere(point3 const &center, double radius, Ray const &r) ;
+static double hit_sphere(point3 const &center, double radius, Ray const &r) ;
 static color ray_color(Ray const &r) ;
 
-int main() {
+int main(int argc, char *argv[]) {
 
 	/*-----------------------------------------------------------------------------
 	 * Image 
@@ -33,7 +33,6 @@ int main() {
 	auto horizontal        = Vec3(viewport_width, 0, 0) ;
 	auto vertical          = Vec3(0, viewport_height, 0) ;
 	auto lower_left_corner = origin - (horizontal/2) - (vertical/2) - Vec3(0, 0, focal_length) ;
-	(void)lower_left_corner ;
 
 	/*-----------------------------------------------------------------------------
 	 * Render 
@@ -41,9 +40,9 @@ int main() {
 
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-	for (int j {image_height-1}; j >= 0; --j) {
+	for (int j {image_height-1}; j >= 0; j--) {
 		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush ;
-		for (int i  {0}; i < image_width; ++i) {
+		for (int i  {0}; i < image_width; i++) {
 			auto u = double(i) / (image_width-1) ;
 			auto v = double(j) / (image_height-1) ;
 			Ray r(origin, lower_left_corner + (u*horizontal) + (v*vertical) - origin) ;
@@ -56,29 +55,31 @@ int main() {
 
 /* ----------------------------------------------------------------------------*/
 /**
-<<<<<<< HEAD
-=======
  * @Brief		- Determines if a light ray hits a sphere by calculating a determinant
  *
  * @Param center	- Center of the sphere
  * @Param radius	- Radius of the sphere
  * @Param r		- Ray we test to see if it hits sphere
  *
- * @Returns bool	- Does the ray hit the sphere?
+ * @Returns double	- Where does the ray hit? Inside or outside?
  */
 /* ------------------------------------------------------------------------------------*/
-static bool hit_sphere(point3 const &center, double radius, Ray const &r) {
-	Vec3 oc           { r.origin() - center} ;				///< origin of circle
-	auto a            { dot(r.direction(), r.direction())} ;		///< <P(t) -C> dot <P(t) - C> = r^2
-	auto b            { 2.0 * dot(oc, r.direction())} ;
-	auto c            { dot(oc, oc) - radius*radius} ;
-	auto discriminant { (b*b) - (4*a*c)} ;
-	return (discriminant > 0) ;
+static double hit_sphere(point3 const &center, double radius, Ray const &r) {
+	Vec3 oc           = r.origin() - center ;				///< origin of circle
+	auto a            = dot(r.direction(), r.direction()) ;		///< <P(t) -C> dot <P(t) - C> = r^2
+	auto b            = 2.0 * dot(oc, r.direction()) ;
+	auto c            = dot(oc, oc) - radius*radius ;
+	auto discriminant = (b*b) - (4*a*c) ;
+	if (discriminant < 0) {
+		return -1.0 ;
+	}
+	else {
+		return (-b - std::sqrt(discriminant) / (2.0*a)) ;
+	}
 }
 
 /* ----------------------------------------------------------------------------*/
 /**
->>>>>>> 05_AddingASphere
  * @Brief		- Traces a color ray
  *
  * @Param r		- The color ray
@@ -91,10 +92,13 @@ static bool hit_sphere(point3 const &center, double radius, Ray const &r) {
  */
 /* ------------------------------------------------------------------------------------*/
 static color ray_color(Ray const &r) {
-	if (hit_sphere(point3(0,0,-1), 0.5, r)) {
-		return color(1, 0, 0) ;
+	auto t = hit_sphere(point3(0,0,-1), 0.5, r) ;
+	if (t > 0.0) {
+		/* Create a surface normal */
+		Vec3 N = unit_vector(r.at(t) - Vec3(0, 0, -1)) ;
+		return ( (0.5*color( N.x()+1, N.y()+1, N.z()+1 )) ) ;
 	}
 	Vec3 unit_direction = unit_vector(r.direction()) ;
-	auto t = 0.5 * (unit_direction.y() + 1.0) ;
+	t = 0.5 * (unit_direction.y() + 1.0) ;
 	return (1.0 - t) * color(1.0, 1.0, 1.0) + (t * color(0.5, 0.7, 1.0)) ;
 }
