a732dd403490c356e011fa487772b81480b44b04
Michael Ly <71558719+Michael-Q-Ly@users.noreply.github.com>
2022-10-05 22:13:32 -0500

    Begin versioning the main.cpp file

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,18 @@
  * @file main.cpp
  * @Brief main program
  * @author Michael Ly (github.com/Michael-Q-Ly)
- * @version 0.0.0
+ * @version 0.0.1
  * @date 2022-10-02
  */
+#include "rtweekend.hpp"
+
 #include "color.hpp"
-#include "Ray.hpp"
-#include "Vec3.hpp"
+#include "Hittable_List.hpp"
+#include "Sphere.hpp"
+
 #include <iostream>
 
-static double hit_sphere(point3 const &center, double radius, Ray const &r) ;
-static color ray_color(Ray const &r) ;
+static color ray_color(Ray const &r, Hittable const &world) ;
 
 int main(int argc, char *argv[]) {
 
@@ -19,8 +21,15 @@ int main(int argc, char *argv[]) {
 	 * Image 
 	 *-----------------------------------------------------------------------------*/
 	auto const aspect_ratio = 16.0 / 9.0 ;
-	int const image_width   = 400 ;
+	int const image_width   = 2560 ;
 	int const image_height  = static_cast<int>(image_width/aspect_ratio) ;
+
+	/*-----------------------------------------------------------------------------
+	 * World 
+	 *-----------------------------------------------------------------------------*/
+	Hittable_List world ;
+	world.add(std::make_shared<Sphere>(point3(0, 0, -1), 0.5)) ;
+	world.add(std::make_shared<Sphere>(point3(0, -100.5, -1), 100)) ;
 
 	/*-----------------------------------------------------------------------------
 	 * Camera 
@@ -46,41 +55,17 @@ int main(int argc, char *argv[]) {
 			auto u = double(i) / (image_width-1) ;
 			auto v = double(j) / (image_height-1) ;
 			Ray r(origin, lower_left_corner + (u*horizontal) + (v*vertical) - origin) ;
-			color pixel_color = ray_color(r) ;
+			color pixel_color = ray_color(r, world) ;
 			write_color(std::cout, pixel_color) ;
         }
     }
 	std::cerr << "\nDone." << std::endl ;
 }
 
-/* ----------------------------------------------------------------------------*/
-/**
- * @Brief		- Determines intersection of ray to a sphere
- *
- * @Param center	- Center of the sphere
- * @Param radius	- Radius of the sphere
- * @Param r		- Ray we test to see if it hits sphere
- *
- * @Returns double	- Where does the ray hit? Inside or outside?
- *
- * @Notes		- Use "simplified" quadratic formula (div by 2/2) for discriminant
- *			\{-b' +/- sqrt[(b')^2 -ac] }/ a,
- *			\where b' = b/2.0
- */
-/* ------------------------------------------------------------------------------------*/
-static double hit_sphere(point3 const &center, double radius, Ray const &r) {
-	Vec3 oc           = r.origin() - center ;				///< origin of circle
-	auto a            = r.direction().length_squared() ;
-	auto half_b       = dot(oc, r.direction()) ;
-	auto c            = oc.length_squared() - (radius*radius) ;
-	auto discriminant = (half_b*half_b) - a*c ;
-	if (discriminant < 0) {
-		return -1.0 ;
-	}
-	else {
-		return ((-half_b - std::sqrt(discriminant)) / a) ;
-	}
-}
+
+/*-----------------------------------------------------------------------------
+ * Functions 
+ *-----------------------------------------------------------------------------*/
 
 /* ----------------------------------------------------------------------------*/
 /**
@@ -95,14 +80,12 @@ static double hit_sphere(point3 const &center, double radius, Ray const &r) {
  *			\3. Computes a color for that intersection point
  */
 /* ------------------------------------------------------------------------------------*/
-static color ray_color(Ray const &r) {
-	auto t = hit_sphere(point3(0,0,-1), 0.5, r) ;
-	if (t > 0.0) {
-		/* Create a surface normal */
-		Vec3 N = unit_vector(r.at(t) - Vec3(0, 0, -1)) ;
-		return ( (0.5*color( N.x()+1, N.y()+1, N.z()+1 )) ) ;
+static color ray_color(Ray const &r, Hittable const &world) {
+	hit_record rec ;
+	if (world.hit(r, 0, infinity, rec)) {
+		return 0.5 * (rec.normal + color(1, 1, 1)) ;
 	}
 	Vec3 unit_direction = unit_vector(r.direction()) ;
-	t = 0.5 * (unit_direction.y() + 1.0) ;
+	auto t = 0.5 * (unit_direction.y() + 1.0) ;
 	return (1.0 - t) * color(1.0, 1.0, 1.0) + (t * color(0.5, 0.7, 1.0)) ;
 }
