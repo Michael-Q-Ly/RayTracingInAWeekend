d2dd8b3a89e6f9605a1dceb3bfeab1d47d26d6d1
Michael Ly <71558719+Michael-Q-Ly@users.noreply.github.com>
2022-10-05 23:43:57 -0500

    Modify main to use anti-aliasing from new Camera and rtweekend files

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,12 @@
  * @file main.cpp
  * @Brief main program
  * @author Michael Ly (github.com/Michael-Q-Ly)
- * @version 0.0.1
+ * @version 0.0.2
  * @date 2022-10-02
  */
 #include "rtweekend.hpp"
 
+#include "Camera.hpp"
 #include "color.hpp"
 #include "Hittable_List.hpp"
 #include "Sphere.hpp"
@@ -20,9 +21,10 @@ int main(int argc, char *argv[]) {
 	/*-----------------------------------------------------------------------------
 	 * Image 
 	 *-----------------------------------------------------------------------------*/
-	auto const aspect_ratio = 16.0 / 9.0 ;
-	int const image_width   = 2560 ;
-	int const image_height  = static_cast<int>(image_width/aspect_ratio) ;
+	auto const aspect_ratio     = 16.0 / 9.0 ;
+	int const image_width       = 2560 ;
+	int const image_height      = static_cast<int>(image_width/aspect_ratio) ;
+	int const samples_per_pixel = 100 ;
 
 	/*-----------------------------------------------------------------------------
 	 * World 
@@ -34,14 +36,7 @@ int main(int argc, char *argv[]) {
 	/*-----------------------------------------------------------------------------
 	 * Camera 
 	 *-----------------------------------------------------------------------------*/
-	auto viewport_height = 2.0 ;
-	auto viewport_width  = aspect_ratio * viewport_height ;
-	auto focal_length    = 1.0 ;
-
-	auto origin            = point3(0, 0, 0) ;
-	auto horizontal        = Vec3(viewport_width, 0, 0) ;
-	auto vertical          = Vec3(0, viewport_height, 0) ;
-	auto lower_left_corner = origin - (horizontal/2) - (vertical/2) - Vec3(0, 0, focal_length) ;
+	Camera cam ;
 
 	/*-----------------------------------------------------------------------------
 	 * Render 
@@ -51,12 +46,15 @@ int main(int argc, char *argv[]) {
 
 	for (int j {image_height-1}; j >= 0; j--) {
 		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush ;
-		for (int i  {0}; i < image_width; i++) {
-			auto u = double(i) / (image_width-1) ;
-			auto v = double(j) / (image_height-1) ;
-			Ray r(origin, lower_left_corner + (u*horizontal) + (v*vertical) - origin) ;
-			color pixel_color = ray_color(r, world) ;
-			write_color(std::cout, pixel_color) ;
+		for (int i {0}; i < image_width; i++) {
+			color pixel_color(0, 0, 0) ;
+			for (int s {0} ; s < samples_per_pixel ; s++) {
+				auto u       = (i + random_double()) / (image_width-1) ;
+				auto v       = (j + random_double()) / (image_height-1) ;
+				Ray r        = cam.get_ray(u, v) ;
+				pixel_color += ray_color(r, world) ;
+			}
+			write_color(std::cout, pixel_color, samples_per_pixel) ;
         }
     }
 	std::cerr << "\nDone." << std::endl ;
