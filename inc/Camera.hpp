ed5eb04f59087faa35a3d325ee3bff87ecf4aecf
Michael Ly <71558719+Michael-Q-Ly@users.noreply.github.com>
2022-10-05 22:31:55 -0500

    Document Camera header and add more utility to color header
    	- Color header now uses clamp function and scales rgb based off
    	  a pass-by-value samples_per_pixel value

--- inc/Camera.hpp
+++ inc/Camera.hpp
@@ -11,15 +11,14 @@
 
 class Camera {
 	public:
-		Camera(void) ;
-
-		Ray get_ray(double u, double v) ;
+		Camera(void) ;				///< No-args constructor
+		Ray get_ray(double u, double v) ;	///< Gets Ray object
 
 	private:
-		point3 origin ;
-		point3 lower_left_corner ;
-		Vec3 horizontal ;
-		Vec3 vertical ;
+		point3 origin ;				///< Origin of Camera object
+		point3 lower_left_corner ;		///< Lower left corner of Camera object
+		Vec3 horizontal ;			///< Horizontal vector of Camera object
+		Vec3 vertical ;				///< Vertical vector of Camera object
 } ;
 
 #endif /* _CAMERA_HPP_ */
